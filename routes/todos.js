c99e21dd502aeef020e7100f3a4ac079c453d0d4
rsimmons11 <ptgrd1980@gmail.com>
2023-09-05 15:07:11 -0700

    updated comments on routes file

--- routes/todos.js
+++ routes/todos.js
@@ -1,7 +1,9 @@
 // This line imports the 'express' library/module and assigns it to the constant variable 'express,' allowing you to create an Express.js application.
 const express = require('express')
+
 // This line creates a new router object using the 'express.Router()' method, which you can use to define routes for your application.
 const router = express.Router()
+
 // Here, you import a module named 'todos' from the '../controllers' directory and assign it to the 'todosController' constant. This module likely contains functions to handle routes related to managing todos.
 const todosController = require('../controllers/todos')
 
